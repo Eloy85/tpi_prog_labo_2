916494b4366c5af723218702d715c2a93939c7ad
Maribel Martino <martino.maribel@gmail.com>
2023-06-17 17:15:43 -0300

    add

--- SocialEventSA/Controlador/ControladorServicio.py
+++ SocialEventSA/Controlador/ControladorServicio.py
@@ -14,3 +14,10 @@ class ControladorServicio:
                 servicio = Servicio(atributos[0], atributos[1], atributos[2], atributos[3])
                 self.servicios.append(servicio)
     
+def lectura_tipos_servicios(servicios):
+    tipos_servicios = []
+    with open(servicios.txt, 'r') as archivo:
+        for linea in archivo:
+            tipo_servicio = linea.strip()
+            tipos_servicios.append(tipo_servicio)
+    return tipos_servicios  
