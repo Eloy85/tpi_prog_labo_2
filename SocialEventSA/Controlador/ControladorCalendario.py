454f5bff7b02910c7dd896478466f3c5a24a7f83
facundopereyra16 <facuu16p@gmail.com>
2023-06-17 18:14:18 -0300

    cambios

--- SocialEventSA/Controlador/ControladorCalendario.py
+++ SocialEventSA/Controlador/ControladorCalendario.py
@@ -1,9 +1,9 @@
 from Modelo.Calendario import Calendario
 from Vista.VistaCalendario import VistaCalendario
+import datetime
+import os
 
 class ControladorCalendario:
     def __init__(self):
         self.vista = VistaCalendario()
-        self.calendario = Calendario()
-
-    
+        self.calendario = Calendario()        
--- SocialEventSA/Vista/VistaCalendario.py
+++ SocialEventSA/Vista/VistaCalendario.py
@@ -1,12 +1,18 @@
 class VistaCalendario:
+    def __init__(self):
+        pass
+    
     def mostrar_mes(self,ruta):
         archivo=open(str(ruta),"r",encoding="utf-8")
         conten=archivo.read()
         archivo.close()
         print(conten) 
+
     def ingresar_año(self):
-        return input("Año a realizar evento: ")
+        return int(input("Año a realizar evento: "))
+
     def ingresar_mes(self):
-        return input("Mes a realizar evento: ")
+        return int(input("Mes a realizar evento: "))
+
     def ingresar_dia(self):
-        return input("Dia a realizar evento: ")
+        return int(input("Dia a realizar evento: "))
