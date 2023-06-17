61214d7cefc9ecabe03eec915c2b9b527c2b30d0
Ignacio Ortiz <nachoortiz154@gmail.com>
2023-06-17 16:31:54 -0300

    TXT con los servicios y controlador de servicio basico

--- SocialEventSA/Controlador/ControladorServicio.py
+++ SocialEventSA/Controlador/ControladorServicio.py
@@ -1,5 +1,16 @@
 from Modelo.Servicio import Servicio
+from Vista.VistaServicio import VistaServicio
 
 class ControladorServicio:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,vista=VistaServicio(),servicio=Servicio(),servicios=[]):
+        self.vista=vista
+        self.servicio=servicio
+        self.servicios=servicios
+    def inicializar_servicios(self):
+        with open("servicios.txt", "r") as archivo:
+            lineas = archivo.readlines()
+            for linea in lineas:
+                atributos = linea.strip().split(';')
+                servicio = Servicio(atributos[0], atributos[1], atributos[2], atributos[3])
+                self.servicios.append(servicio)
+    
