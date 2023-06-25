ce313d6ead53ac3632b55eb5683f214881aa644b
facundopereyra16 <facuu16p@gmail.com>
2023-06-25 12:27:11 -0300

    cambios en detalleEvento

--- SocialEventSA/Controlador/ControladorDetalleEvento.py
+++ SocialEventSA/Controlador/ControladorDetalleEvento.py
@@ -1,5 +1,6 @@
 from Modelo.DetalleEvento import DetalleEvento
 from Modelo.Evento import Evento
+from Modelo.Servicio import Servicio
 from Vista.VistaDetalleEvento import VistaDetalleEvento
 
 class ControladorDetalleEvento:
@@ -13,8 +14,8 @@ class ControladorDetalleEvento:
         for objeto in lista:
             precio = objeto.GetPrecio() #traer get del modelo de servicios
 
-    def cargar_detalle_de_clientes(self, nombre_cliente):
-        with open(f"C:\\Users\\facuu\\OneDrive\\Escritorio\\Integrador\\tpi_prog_labo_2\\SocialEventSA\\Detalle Clientes\\D_{nombre_cliente}.txt", 'w') as archivo:
+    def cargar_detalle_de_clientes(self, nombre_cliente, fecha_evento):
+        with open(f"Detalle Clientes\\D_{fecha_evento}-{nombre_cliente}.txt", 'w') as archivo:
             self.detalleEvento.calcularIva()
             archivo.write(f"Cliente: {self.evento.getCliente()}")
             archivo.write(f"Evento: {self.evento.getTipoEvento()}")
@@ -22,8 +23,6 @@ class ControladorDetalleEvento:
             archivo.write("Servicios seleccionados:")
             for servicio in self.evento.getServicios():
                 archivo.write(f"{servicio.getTipoServicio()} - ${servicio.getPrecio()}")
-            archivo.write("--------------------------")
-            archivo.write("Conceptos a abonar:")
             archivo.write(f"Costo administrativo: ${self.detalleEvento.getCostoAdministrativo()}")
             archivo.write(f"Servicios seleccionados: ${self.detalleEvento.getCostoServicios()}")
             archivo.write(f"IVA: ${self.detalleEvento.getIva()}")
