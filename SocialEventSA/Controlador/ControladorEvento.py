757100f950091c9562497a3940e800d11dc625fe
Nachoo154 <63886703+Nachoo154@users.noreply.github.com>
2023-06-25 21:18:21 -0300

    Borre lo de modificar precios

--- SocialEventSA/Controlador/ControladorEvento.py
+++ SocialEventSA/Controlador/ControladorEvento.py
@@ -283,23 +283,7 @@ class ControladorEvento:
                         controladorCliente.consultarCliente()
                     elif opcionClientes == 3:
                         controladorCliente.modificarCliente()
+            
             elif opcion == 3:
-                opcionCostos = 0
-                while opcionCostos != 4:
-                    var1=1
-                    while var1<2:
-                        try:
-                            opcionCostos = self.vista.costosPrecios()
-                            var1=3  
-                        except ValueError:
-                            self.vista.valorIncorrecto()
-                    
-                    while opcionCostos < 1 or opcionCostos > 4:
-                        opcionCostos = self.vista.costosPrecios()
-                    if opcionCostos == 1:
-                        opcionAdmin = self.vista.costoAdministrativo()
-                        if opcionAdmin.upper() == "S":
-                            self.detalleEvento.setCostoAdministrativo(self.vista.nuevoPrecio())
-            elif opcion == 4:
                 self.vista.cerrando_programa()
                 self.vista.limpiar_pantalla()
